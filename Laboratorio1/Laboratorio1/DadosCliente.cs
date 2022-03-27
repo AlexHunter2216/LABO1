f21ec801e9c0322edf6d512f32fa31e6674bc1b9
AlexHunter2216 <70276395+AlexHunter2216@users.noreply.github.com>
2022-03-27 17:13:00 -0300

    Add files via upload

---
+++ Laboratorio1/Laboratorio1/DadosCliente.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio1
+{
+    class DadosCliente
+    {
+
+        public string Nome;
+        public int Idade;
+        public double IMC;
+        public double Altura; 
+        public double Peso;
+
+        //Metodo
+        public void Situação()
+        {
+            if (IMC >= 18.5 && IMC <= 24.9)
+            {
+                Console.WriteLine("Calculando sua situação");
+                Console.WriteLine("...");
+                Console.WriteLine("Sua situação é: Parabens você está no peso ideal");
+            }
+            else if (IMC >= 25.0 && IMC <= 29.9 )
+            {
+                Console.WriteLine("Calculando sua situação");
+                Console.WriteLine("...");
+                Console.WriteLine("Sua situação é: Você está acima de seu peso (sobrepeso).");
+                Console.WriteLine("...");
+            }
+            else if(IMC >= 30.0 && IMC <= 34.9)
+            {
+                Console.WriteLine("Calculando sua situação");
+                Console.WriteLine("...");
+                Console.WriteLine("Sua situação é: Obesidade grau I.");
+                Console.WriteLine("...");
+            }
+            else if (IMC >= 35.0 && IMC <= 39.9)
+            {
+                Console.WriteLine("Calculando sua situação");
+                Console.WriteLine("...");
+                Console.WriteLine("Suasituação é: Obesidade grau II.");
+                Console.WriteLine("...");
+            }
+            else if (IMC >= 40.0)
+            {
+                Console.WriteLine("Calculando sua situação");
+                Console.WriteLine("...");
+                Console.WriteLine("Sua situação é: Obesidade graus III e IV.");
+                Console.WriteLine("...");
+            }
+        }
+
+
+       public void CalculoIMC()
+        {
+            double Alt = Math.Pow(Altura, 2);
+            double IMC1 = Peso / Alt;           
+            double IMC2 = Math.Round(IMC1);
+            Console.WriteLine($"Seu IMC é: {IMC2}");
+            IMC = IMC2;
+
+        }
+
+        public void Meta()
+        {
+            if (IMC > 24.9)
+            {
+                double IMCMeta = IMC - 24.9d;
+                double Altura2 = Math.Pow(Altura, 2);
+                double META = IMCMeta * Altura2;
+                double META2 = Math.Round(META);
+                Console.WriteLine($"Para vc estar no peso ideal é necessario perder: {META2} Quilos.");
+            }
+            else
+            {
+                Console.WriteLine("Continue assim. ^-^ ");
+            }
+        }
+    }
+
+    
+
+}
+
---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadosCliente Cliente = new DadosCliente();
+
+            Console.WriteLine("Insira seu Nome: ");
+            string Nome1 = Console.ReadLine();
+            Cliente.Nome = Nome1;
+            Console.WriteLine("...");
+            Console.WriteLine("Insira sua idade: ");
+            int Idade1 = Convert.ToInt32(Console.ReadLine());
+            Cliente.Idade = Idade1;
+            Console.WriteLine("...");
+            Console.WriteLine("Insira a sua altura: ");
+            double Altura1 = Convert.ToDouble(Console.ReadLine());
+            Cliente.Altura = Altura1;
+            Console.WriteLine("...");
+            Console.WriteLine("Insira seu peso: ");
+            double Peso1 = Convert.ToDouble(Console.ReadLine());
+            Cliente.Peso = Peso1;
+            Console.WriteLine($"Nome do Cliente: {Nome1}");
+            Console.WriteLine("");
+            Console.WriteLine($"Idade do Cliente: {Idade1}");
+            Console.WriteLine("");
+            Console.WriteLine($"Altura do Cliente: {Altura1}");
+            Console.WriteLine("");
+            Console.WriteLine($"Peso do Cliente: {Peso1}");
+            Console.WriteLine($"...");
+            Cliente.CalculoIMC();
+            Console.WriteLine("");
+            Cliente.Situação();
+            Console.WriteLine("");
+            Cliente.Meta();
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
